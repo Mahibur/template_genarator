4c82fec41a68ef7449dcf55aa40a029733700f80
Mahibur <mmahibur10100@gmail.com>
2025-03-09 23:28:30 +0600

    nothing really special

--- src/components/pages/GoldAmbassador.jsx
+++ src/components/pages/GoldAmbassador.jsx
@@ -7,7 +7,7 @@ const GoldAmbassador = () => {
 
   return (
     <div>
-        <Heading text="Global Enterpreneur" className="bg-[#D39332]"/>
+        <Heading text="Gold Ambassador" className="bg-[#D39332]"/>
         <InputBox imgClass="bg-[url(/9.jpg)]" className="absolute top-1/2 left-1/2 translate-x-[-53%] translate-y-[-24%] block w-70.5 rounded-[50%]"/>
     </div>
   );
--- src/components/pages/PlatinumAmbassador.jsx
+++ src/components/pages/PlatinumAmbassador.jsx
@@ -5,9 +5,6 @@ import InputBox from "../InputBox";
 const PlatinumAmbassador = () => {
   return (
     <div>
-      <h1 className="bg-[#8AFF21] text-3xl font-bold text-center py-6">
-      Platinum Ambassador
-        </h1>
         <Heading text="Platinum Ambassador" className="bg-[#8AFF21]"/>
         <InputBox imgClass="bg-[url(/10.jpg)]" className="absolute top-1/2 left-1/2 translate-x-[-53%] translate-y-[-24%] block w-70.5 rounded-[50%]"/>
     </div>
