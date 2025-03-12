1af3a0d55804efcee93581f073e7f1e521ff4ecc
Mahibur <mmahibur10100@gmail.com>
2025-03-12 21:28:42 +0600

    hello

--- src/components/pages/RockStar.jsx
+++ src/components/pages/RockStar.jsx
@@ -8,7 +8,7 @@ const RockStar = () => {
       <Heading text="Rockstar Award" className="bg-[#ECCE71]" />
       <InputBox
         imgClass="bg-[url(/19.jpeg)] w-[1080px] h-[1080px]"
-        className="absolute top-1/2 right-[5%] translate-x-[-1%] translate-y-[-57%] block w-104 rounded-[50%]"
+        className="absolute top-1/2 right-[5%] translate-x-[-1%] translate-y-[-57%] block w-93 rounded-[50%]"
         textClass="top-[68%] right-0 translate-x-[24%] translate-y-[5%] text-[#121212]"
       />
     </div>
